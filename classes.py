cc280e8e84de60edb5b7db418e30b0d85fc88e92
MARAT <almat59@rambler.ru>
2024-02-18 22:26:37 +0300

    Initial commit

---
+++ classes.py
@@ -0,0 +1,28 @@
+class Category:
+    name: str
+    description: str
+    goods: str
+
+    count_category = 0
+    count_unique_products = 0
+
+    def __init__(self, name, description, goods):
+        self.name = name
+        self.description = description
+        self.goods = goods
+
+        Category.count_category += 1
+        Category.count_unique_products += 1
+
+
+class Product:
+    name: str
+    description: str
+    price: float
+    quantity: int
+
+    def __init__(self, name, description, price, quantity):
+        self.name = name
+        self.description = description
+        self.price = price
+        self.quantity = quantity
---
+++ test_class.py
@@ -0,0 +1,30 @@
+import pytest
+
+from classes import Category, Product
+
+@pytest.fixture
+def class_category():
+    return Category('Телефон', 'Мощный', 'iPhone')
+
+def test_category_init(class_category):
+    assert class_category.name == 'Телефон'
+    assert class_category.description == 'Мощный'
+    assert class_category.goods == 'iPhone'
+
+
+def test_count():
+    assert Category.count_category == 1
+    assert Category.count_unique_products == 1
+
+@pytest.fixture
+def class_product():
+    return Product('Tele2', 'Оператор связи', 299.0, 100)
+
+def test_product_init(class_product):
+    assert class_product.name == 'Tele2'
+    assert class_product.description == 'Оператор связи'
+    assert class_product.price == 299.0
+    assert class_product.quantity == 100
+
+
+
